93a068d594933262ed4279bd08d4df68b80f9234
Airyz <36567925+Airyzz@users.noreply.github.com>
2024-04-18 10:05:56 +0930

    Revert "update formatting"
    This reverts commit 10f72089c68a6d9a2383ff5bc22b96aadf842310.

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,8 +1,8 @@
 export default {
-	joinArray(a: Uint8Array, b: Uint8Array): Uint8Array {
-		var mergedArray = new Uint8Array(a.length + b.length);
-		mergedArray.set(a);
-		mergedArray.set(b, a.length);
-		return mergedArray
-	}
+    joinArray(a: Uint8Array, b: Uint8Array): Uint8Array {
+        var mergedArray = new Uint8Array(a.length + b.length);
+        mergedArray.set(a);
+        mergedArray.set(b, a.length);
+        return mergedArray
+    }
 }
